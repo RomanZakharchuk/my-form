c6d712036b89b0092b75148f191df8b13e9c3d83
Roman Zakharchuk <romanzakharchuk32@gmail.com>
2020-10-12 15:32:30 +0300

    add function

--- src/App.js
+++ src/App.js
@@ -7,28 +7,41 @@ function App() {
       <form>
         <label>
           Name:
-          <input type="text" name="name" />
+          <input type="text" name="name" id="my-name" />
         </label>
         <br />
         <label>
           Email:
-          <input type="email" name="email" />
+          <input type="email" name="email" id="my-email" />
         </label>
         <br />
         <label>
           Password:
-          <input type="password" name="password1" />
+          <input type="password" name="password1" id="pass1" />
         </label>
         <br />
         <label>
           Password:
-          <input type="password" name="password2" />
+          <input type="password" name="password2" id="pass2" />
         </label>
         <br />
-        <input type="submit" value="Send" />
+        <input type="submit" value="Send" onClick={sendName} />
       </form>
     </div>
   );
+}
+
+function sendName() {
+  const nameValue = document.getElementById("my-name");
+  const emailValue = document.getElementById("my-email");
+  const pass1 = document.getElementById("pass1");
+  const pass2 = document.getElementById("pass2");
+
+  if (pass1.value != pass2.value) {
+    alert("Error!");
+  }
+
+  alert("Name: " + nameValue.value + " Email: " + emailValue.value);
 }
 
 export default App;
