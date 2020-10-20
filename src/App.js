c0d2f1217624fa536cfa824f5cc232d644a0df41
Roman Zakharchuk <romanzakharchuk32@gmail.com>
2020-10-20 22:33:37 +0300

    corected

--- src/App.js
+++ src/App.js
@@ -1,47 +1,95 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="forms">
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" id="my-name" />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input type="email" name="email" id="my-email" />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" name="password1" id="pass1" />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" name="password2" id="pass2" />
-        </label>
-        <br />
-        <input type="submit" value="Send" onClick={sendName} />
-      </form>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", password_one: "", password_two: "" };
 
-function sendName() {
-  const nameValue = document.getElementById("my-name");
-  const emailValue = document.getElementById("my-email");
-  const pass1 = document.getElementById("pass1");
-  const pass2 = document.getElementById("pass2");
-
-  if (pass1.value != pass2.value) {
-    alert("Error!");
+    this.onChangePassOne = this.onChangePassOne.bind(this);
+    this.onChangePassTwo = this.onChangePassTwo.bind(this);
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  alert("Name: " + nameValue.value + " Email: " + emailValue.value);
+  onSubmit(event) {
+    alert(`Your Name:  ${this.state.name}
+    Your Email: ${this.state.email}`);
+
+    if (`${this.state.password_one}` != `${this.state.password_two}`) {
+      alert("Your password incorrect!");
+    } else {
+      alert("Congratulations!");
+    }
+    event.preventDefault();
+  }
+
+  onChangePassOne = (event) => {
+    this.setState({ password1: event.target.value });
+  };
+
+  onChangePassTwo = (event) => {
+    this.setState({ password2: event.target.value });
+  };
+
+  onChangeEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  onChangeName = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="forms">
+        <form onSubmit={this.onSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.value}
+              onChange={this.onChangeName}
+            />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={this.state.value}
+              onChange={this.onChangeEmail}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password_one"
+              value={this.state.value}
+              onChange={this.onChangePassOne}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password_two"
+              value={this.state.value}
+              onChange={this.onChangePassTwo}
+            />
+          </label>
+          <br />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
